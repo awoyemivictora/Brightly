cd5146f016ca98bf18eda8b30bd39b0aca814536
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-12-30 21:55:48 +0100

    NEW

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import '../styles/main.css'
+import '../styles/responsive.css'
 import type { AppProps } from "next/app";
 import { MeshProvider } from "@meshsdk/react";
 
