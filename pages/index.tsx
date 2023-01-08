a1c792ab488241d3a1cc2ada7f0ae3b1e36aa14c
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-08 01:57:00 +0100

    TESTING IFRAME

--- pages/index.tsx
+++ pages/index.tsx
@@ -425,14 +425,28 @@ const Party: NextPage = () => {
               <div className="line_left">
                 <img src="./img/perk.png" alt="" />
               </div>
-              <div className="line_right" style="overflow-y:scroll;">
+              <div className="line_right">
                 <p>
                   Own 1 x BB <a href="#">main collection</a>
                 </p>
               </div>
             </div>
-
-            <iframe 
+            
+                                <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
+                    <script type="text/javascript">
+                    $(function(){
+                        $('#button').click(function(){ 
+                            if(!$('#iframe').length) {
+                                    $('#iframeHolder').html('<iframe id="iframe" src="https://notionforms.io/forms/classpass" width="700" height="450"></iframe>');
+                            }
+                        });   
+                    });
+                    </script>
+                    
+                    <button id="button">Button</button>
+                    <div id="iframeHolder"></div>
+            
+            {/* <iframe 
             src="https://notionforms.io/forms/classpass" 
             name="iframe1"
             title="CLAIM PERK"
@@ -443,7 +457,7 @@ const Party: NextPage = () => {
             target="iframe1" 
             className="claim">
               claim perk
-            </a>
+            </a> */}
             
            
             
